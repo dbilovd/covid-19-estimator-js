d202943ccd4b79c5729c85eb73ffc6201806bbbf
David Lartey <dbilovd@gmail.com>
2020-04-07 23:17:59 +0000

    minor: Updated files to using 2 spaces

--- src/estimator.js
+++ src/estimator.js
@@ -1,38 +1,40 @@
+'use strict';
+
 import Helpers from './helpers'
 
 const CURRENTLY_INFECTED_FACTOR = 10;
 const CURRENTLY_INFECTED_SEVERE_FACTOR = 50;
 
 const calculateCurrentlyInfected = (cases, severe) => {
-    let factor = severe ? CURRENTLY_INFECTED_SEVERE_FACTOR : CURRENTLY_INFECTED_FACTOR;
-    
-    return cases * factor;
+  let factor = severe ? CURRENTLY_INFECTED_SEVERE_FACTOR : CURRENTLY_INFECTED_FACTOR;
+
+  return cases * factor;
 }
 
 const calculateEstimatedInfectionsByDays = (currentInfections, days, severe) => {
-    let incrementFactor = Math.floor(days / 3);
-    return currentInfections * Math.pow(2, incrementFactor);
+  let incrementFactor = Math.floor(days / 3);
+  return currentInfections * Math.pow(2, incrementFactor);
 }
 
 const covid19ImpactEstimator = (data) => {
-    let currentlyInfected = calculateCurrentlyInfected(data.reportedCases)
-    let currentlyInfectedSevere = calculateCurrentlyInfected(data.reportedCases, true);
+  let currentlyInfected = calculateCurrentlyInfected(data.reportedCases)
+  let currentlyInfectedSevere = calculateCurrentlyInfected(data.reportedCases, true);
 
-    let numberOfDays = Helpers.getTimeElapsedInDays(28, "days");
-    let infectionsByRequestedTime = calculateEstimatedInfectionsByDays(currentlyInfected, numberOfDays);
-    let infectionsByRequestedTimeSevere = calculateEstimatedInfectionsByDays(currentlyInfectedSevere, numberOfDays, true);
+  let numberOfDays = Helpers.getTimeElapsedInDays(28, "days");
+  let infectionsByRequestedTime = calculateEstimatedInfectionsByDays(currentlyInfected, numberOfDays);
+  let infectionsByRequestedTimeSevere = calculateEstimatedInfectionsByDays(currentlyInfectedSevere, numberOfDays, true);
 
-    return {
-        data,
-        impact: {
-            currentlyInfected,
-            infectionsByRequestedTime,
-        },
-        severeImpact: {
-            currentlyInfected: currentlyInfectedSevere,
-            infectionsByRequestedTime: infectionsByRequestedTimeSevere,
-        }
+  return {
+    data,
+    impact: {
+      currentlyInfected,
+      infectionsByRequestedTime,
+    },
+    severeImpact: {
+      currentlyInfected: currentlyInfectedSevere,
+      infectionsByRequestedTime: infectionsByRequestedTimeSevere,
     }
+  }
 };
 
 export default covid19ImpactEstimator;
--- src/helpers.js
+++ src/helpers.js
@@ -1,24 +1,25 @@
+'use strict';
 
 let getTimeElapsedInDays = (duration, quantity) => {
-    switch (quantity) {
-        case 'weeks':
-        case 'week':
-            return duration * 7;
-            break;
+  switch (quantity) {
+    case 'weeks':
+    case 'week':
+      return duration * 7;
+      break;
 
-        case 'months':
-        case 'month':
-            return duration * 30;
-            break;
-    
-        case 'days':
-        case 'day':
-        default:
-            return duration;
-            break;
-    }
+    case 'months':
+    case 'month':
+      return duration * 30;
+      break;
+
+    case 'days':
+    case 'day':
+    default:
+      return duration;
+      break;
+  }
 }
 
 export default {
-    getTimeElapsedInDays,
+  getTimeElapsedInDays,
 }
