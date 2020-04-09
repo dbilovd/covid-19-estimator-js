80a7bcffcd0ed020181be611d90338ef06d7401d
David Lartey <dbilovd@gmail.com>
2020-04-09 10:45:50 +0000

    debugging hospital beds bug

--- src/estimator.js
+++ src/estimator.js
@@ -80,10 +80,10 @@ const covid19ImpactEstimator = (data) => {
       currentlyInfected: formatDataForResponse(currentlyInfected),
       infectionsByRequestedTime: formatDataForResponse(infectionsByRequestedTime),
       severeCasesByRequestedTime: formatDataForResponse(severeCasesByRequestedTime),
-      // hospitalBedsByRequestedTime: formatDataForResponse(hospitalBedsByRequestedTime),
-      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime < 0
-        ? Math.round(hospitalBedsByRequestedTime)
-        : formatDataForResponse(hospitalBedsByRequestedTime),
+      hospitalBedsByRequestedTime: formatDataForResponse(hospitalBedsByRequestedTime),
+      // hospitalBedsByRequestedTime: hospitalBedsByRequestedTime < 0
+      //   ? Math.round(hospitalBedsByRequestedTime)
+      //   : formatDataForResponse(hospitalBedsByRequestedTime),
       casesForICUByRequestedTime: formatDataForResponse(casesForICUByRequestedTime),
       casesForVentilatorsByRequestedTime: formatDataForResponse(
         casesForVentilatorsByRequestedTime
@@ -94,10 +94,10 @@ const covid19ImpactEstimator = (data) => {
       currentlyInfected: formatDataForResponse(currentlyInfectedSevere),
       infectionsByRequestedTime: formatDataForResponse(infectionsByRequestedTimeSevere),
       severeCasesByRequestedTime: formatDataForResponse(severeCasesByRequestedTimeSevere),
-      // hospitalBedsByRequestedTime: formatDataForResponse(hospitalBedsByRequestedTimeSevere),
-      hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeSevere < 0
-        ? Math.round(hospitalBedsByRequestedTimeSevere)
-        : formatDataForResponse(hospitalBedsByRequestedTimeSevere),
+      hospitalBedsByRequestedTime: formatDataForResponse(hospitalBedsByRequestedTimeSevere),
+      // hospitalBedsByRequestedTime: hospitalBedsByRequestedTimeSevere < 0
+      //   ? Math.round(hospitalBedsByRequestedTimeSevere)
+      //   : formatDataForResponse(hospitalBedsByRequestedTimeSevere),
       casesForICUByRequestedTime: formatDataForResponse(casesForICUByRequestedTimeSevere),
       casesForVentilatorsByRequestedTime: formatDataForResponse(
         casesForVentilatorsByRequestedTimeSevere
