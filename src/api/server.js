45636951f4dcf2359cc933cb04b6d9de9b7919de
David Lartey <dbilovd@gmail.com>
2020-04-10 00:19:58 +0000

    append new line to file

--- src/api/server.js
+++ src/api/server.js
@@ -19,16 +19,10 @@ const logger = (req, res, next) => {
     const entry = [req.method, req.originalUrl, res.statusCode, `${timeDiff} ms`]
       .join('\t\t');
 
-    fs.open('logs.txt', 'w', (err, fd) => {
+    fs.appendFile('logs.txt', `\n${entry}`, (err) => {
       if (err) {
         return false;
       }
-
-      const buffer = Buffer.from(entry);
-      fs.write(fd, buffer, 0, buffer.length, null, () => {
-        fs.close(fd, () => {});
-      });
-
       return true;
     });
   });
